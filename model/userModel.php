9e7b3602254a0599d9041d026d15fdd77a6e5a85
vaichu <vaishnavvaichu123@gmail.com>
2020-11-29 03:09:10 +0530

    Added_prepared_stmt

---
+++ model/userModel.php
@@ -0,0 +1,46 @@
+<?php
+class userModel{
+    private $u_name;
+    private $p_word;
+    protected $logged_in = false;
+    function __construct($uname,$pass)
+    {
+        $this->u_name=$uname;
+        $this->p_word=$pass;
+        $this->logged_in = false;
+    }
+     public function login(dbconnModel $con)
+    {   
+        $stmp = $this->db->prepare("SELECT * FROM user WHERE USER_ID=? AND PASSWORD=?");
+        $stmp->bind_param("ss",$u,$p);
+        $u = $this->u_name;
+        $p = $this->p_word;
+        $stmp->execute();
+		$res = $stmp->get_result();
+        $stmp->close();
+        $con->db->close();
+        if($res->num_rows)
+        {
+           $this->logged_in = true;
+        }
+        else
+        {
+            $this->logged_in = false;
+        }
+        
+    }
+    function show_id()
+    {
+        return $this->logged_in;
+    }
+
+    public function logout(){
+        if($_SESSION['login']){
+        $_SESSION['login'] = false;
+        return true;
+        }
+        else
+            return false;
+    }
+}
+?>
