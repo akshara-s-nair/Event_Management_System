0958c805dffcf13a4b2780e22fc2f1eaf0a99432
Anugeeth <anugeeth2@gmail.com>
2020-11-29 03:07:24 +0530

    nothing much

--- view/add.php
+++ view/add.php
@@ -10,31 +10,66 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
+<div class="row">
+
 <form>
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-6">
       <label for="fname">First Name</label>
       <input type="text" class="form-control" id="firstname" placeholder="First Name">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-sm-6">
       <label for="lname">Last Name</label>
       <input type="text" class="form-control" id="lastname" placeholder="Last Name">
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-row">
+  <div class="form-group col-sm-6">
     <label for="inputAddress">Aadhaar</label>
     <input type="text" class="form-control" id="aadhaar" placeholder="1234 5678 9870">
   </div>
-  <div class="form-group">
-    <label for="inputAddress2">House Name</label>
-    <input type="text" class="form-control" id="address" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-group">
+  <div class="form-group col-sm-6">
     <label for="inputAddress2">Mobile Number</label>
     <input type="text" class="form-control" id="mobile" placeholder="9876543210">
   </div>
+  </div>
+ 
+  <div class="form-group">
+    <label for="inputAddress2">House Name</label>
+    <input type="text" class="form-control" id="address" placeholder="Apartment, or floor">
+  </div>
 
-  <button type="submit" class="btn btn-primary">Sign in</button>
+<!-- venue -->
+<div class="form-row">
+    <div class="form-group col-6">
+      <label for="fname"> Name</label>
+      <input type="text" class="form-control" id="venuename" placeholder="Name">
+    </div>
+    <div class="form-group col-sm-6">
+      <label for="lname">City</label>
+      <input type="text" class="form-control" id="venuecity" placeholder="City">
+    </div>
+
+    <div class="form-group col-6">
+      <label for="fname"> Max Capacity</label>
+      <input type="text" class="form-control" id="firstname" placeholder="Max Capacity">
+    </div>
+    <div class="form-group col-sm-6">
+      <label for="lname">Cost per hour</label>
+      <input type="text" class="form-control" id="lastname" placeholder="Cost per hour">
+    </div>
+
+
+</div>
+
+
+
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+
 </form>
+</div>
+</div>
 </body>
 </html>
