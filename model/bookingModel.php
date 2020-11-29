57388d8a0e5dfb57f6d9492584824aad309dbf1c
Anugeeth <anugeeth2@gmail.com>
2020-11-29 10:44:21 +0530

    minor ui changes

--- model/bookingModel.php
+++ model/bookingModel.php
@@ -5,18 +5,17 @@ class bookingModel{
     private $time;
     private $no_of_people;
 
-    function __construct( $booking_id,$cust_id,$date,$time,$no_of_people)
+    function __construct( $booking_id,$cust_id,$date,$time)
     {
         $this->booking_id=$booking_id;
         $this->cust_id=$cust_id;
         $this->date = $date;
         $this->time = $time;
-        $this->no_of_people= $no_of_people;
 
     }
 
     public function newBooking(dbconnModel $con){
-        $stmp = $con->prepare("INSERT INTO booking (CUST_ID, DATE_EVENT, START_TIME, NO_OF_PEOPLE) VALUES (?, ?, ?, ?)");
+        $stmp = $con->prepare("INSERT INTO booking (CUST_ID, DATE_EVENT, START_TIME) VALUES (?, ?, ?)");
         $stmp->bind_param("ssss",$cust_id,$date,$time,$no_of_people);
         if ($stmp->execute()){
             returnId($con);
---
+++ model/photoModel.php
@@ -0,0 +1,24 @@
+<?php
+class photoModel{
+    private $no;
+    private $cost;
+    function __construct( $no,$date)
+    {
+        $this->no = $no;
+        $this->cost = $cost;
+    }
+    public function newBooking(dbconnModel $con){
+        $stmp = $con->prepare("INSERT INTO photography ( NO_OF_PHOTOGRAPHERS, PHOTO_COST) VALUES (?, ?)");
+        $stmp->bind_param("ss",$no,$cost);
+        if ($stmp->execute()){
+            return 'true';
+        }
+        else{
+            return 'false';
+        }
+        $stmp->close();
+        
+    }
+}
+
+?>
