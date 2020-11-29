8f9f09761e97f17d398f954b4c30ecceff58a75e
vaichu <vaishnavvaichu123@gmail.com>
2020-11-29 11:49:31 +0530

    .

--- model/bookingModel.php
+++ model/bookingModel.php
@@ -3,21 +3,22 @@ class bookingModel{
     private $cust_id;
     private $date;
     private $time;
+    private $no_of_people;
+    private $booking_id;
 
-    function __construct( $booking_id,$cust_id,$date,$time)
+    function __construct( $cust_id,$date,$time,$no_of_people)
     {
-        $this->booking_id=$booking_id;
         $this->cust_id=$cust_id;
         $this->date = $date;
         $this->time = $time;
+        $this->no_of_people= $no_of_people;
 
     }
-
-    public function newBooking(dbconnModel $con){
-        $stmp = $con->prepare("INSERT INTO booking (CUST_ID, DATE_EVENT, START_TIME) VALUES (?, ?, ?)");
-        $stmp->bind_param("ssss",$this->cust_id,$this->date,$this->time);
+    public function newBooking($con){
+        $stmp = $con->prepare("INSERT INTO booking (CUST_ID, DATE_EVENT, START_TIME, NO_OF_PEOPLE) VALUES (?, ?, ?, ?, ?)");
+        $stmp->bind_param("ssss",$cust_id,$date,$time,$no_of_people);
         if ($stmp->execute()){
-            returnId($con);
+            $this->booking_id = returnId($con);
         }
         else{
             return false;
@@ -26,12 +27,14 @@ class bookingModel{
         
     }
 
-    public function returnId(dbconnModel $con){
+    private function returnId($con){
         $s = $con->prepare("SELECT BOOKING_ID FROM booking WHERE CUST_ID = ?");
         $s->bind_param("s",$this->cust_id);
         $s->execute();
 		$res = $s->get_result();
         $s->close();
+        $res_array = $res->fetch_assoc();
+        return $res_array['BOOKING_ID'];
         
     }
 
